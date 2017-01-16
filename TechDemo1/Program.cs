f26512b36b2ea56e718292d2c66458784400efde
Kyle <Kyle@Berezin.com>
2017-01-15 22:20:08 -0500

    Basic Console

--- TechDemo1/Program.cs
+++ TechDemo1/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console = SadConsole.Consoles.Console;
+using SadConsole.Consoles;
+using Microsoft.Xna.Framework;
 
 namespace TechDemo1
 {
@@ -10,7 +13,29 @@ namespace TechDemo1
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Hello World!");
+            // Setup the engine and creat the main window.
+            SadConsole.Engine.Initialize("IBM.font", 80, 25);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Engine.EngineStart += Engine_EngineStart;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Engine.EngineUpdated += Engine_EngineUpdated;
+
+            // Start the game.
+            SadConsole.Engine.Run();
+        }
+
+        private static void Engine_EngineStart(object sender, EventArgs e)
+        {
+            var defaultConsole = (Console)SadConsole.Engine.ActiveConsole;
+
+            defaultConsole.Print(1, 1, "Hello World!", Color.Aqua, Color.Black);
+        }
+
+        private static void Engine_EngineUpdated(object sender, EventArgs e)
+        {
+
         }
     }
 }
