dac53079b209e32324ab23b68048a355dc560a28
whatt <whatt@LAPTOP-5JHEPDTM>
2017-01-17 01:24:14 -0500

    Split server into new project

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Lidgren;
 
-namespace TechDemo1.NetworkClient
+namespace GameServer
 {
-    class GameServer
+    class Program
     {
         static void Main(string[] args)
         {
-            if(args.Length != 1)
+            if (args.Length != 1)
             {
                 Console.WriteLine("USAGE: GameServer.exe [port]");
                 Console.ReadKey();
@@ -22,7 +21,6 @@ namespace TechDemo1.NetworkClient
             //TODO: Init Incoming Data Handler thread
             //TODO: Init Outgoing Data Handler thread
             //TODO: Init Simulation(?)
-
         }
     }
 }
