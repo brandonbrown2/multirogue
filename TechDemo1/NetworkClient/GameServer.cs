5184b5dd577b5f6c185e28da56c53e74785bf416
whatt <whatt@LAPTOP-5JHEPDTM>
2017-01-17 00:49:42 -0500

    Created dummy GameServer class

---
+++ TechDemo1/NetworkClient/GameServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren;
+
+namespace TechDemo1.NetworkClient
+{
+    class GameServer
+    {
+        static void Main(string args[])
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("USAGE: GameServer.exe [port]");
+                Console.ReadKey();
+                return;
+            }
+            //TODO: Create Map
+            //TODO: Init Server thread
+            //TODO: Init Incoming Data Handler thread
+            //TODO: Init Outgoing Data Handler thread
+            //TODO: Init Simulation(?)
+
+        }
+    }
+}
