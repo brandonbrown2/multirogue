1d5e76a112b76256e20ddf43b1f723d505b89aa2
Kyle <Kyle@Berezin.com>
2017-01-17 04:46:47 -0500

    Slightly less broken

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -13,48 +13,27 @@ namespace GameServer
 {
     class Program
     {
-        static private int port;
+        static public int port = 2025;
         static private NetServer server;
         static private Thread clientHandler;
         static private int mapSeed;
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("USAGE: GameServer.exe [port]");
-                Console.ReadKey();
-                return;
-            }
-            
-            if (!Int32.TryParse(args[0], out port))
-            {
-                Console.WriteLine("Bad port argument!");
-                Console.ReadKey();
-            }
-            else if (port > 0)
-            {
-                //TODO: Create Map
-                //TODO: Init Outgoing Data Handler thread
-                //TODO: Init Simulation(?)
-                Console.WriteLine("Good port!");
-                var randomgen = new RogueSharp.Random.DotNetRandom();
-                mapSeed = randomgen.Next(Int32.MaxValue - 1);
-                Console.WriteLine("Map Generated!");
-                var config = new NetPeerConfiguration("Multirogue Server") { Port = port };
-                server = new NetServer(config);
-                server.Start();
-                Console.WriteLine("Server Initialized!");
-                ClientHandler c = new ClientHandler(server, mapSeed);
-                clientHandler = new Thread(c.ClientMessageRecieverThread);
-                clientHandler.Start();
-                Console.WriteLine("Client Handler Initialized!");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Port must be greater than zero!");
-                Console.ReadKey();
-            }
+            //TODO: Create Map
+            //TODO: Init Outgoing Data Handler thread
+            //TODO: Init Simulation(?)
+            Console.WriteLine("Good port!");
+            var randomgen = new RogueSharp.Random.DotNetRandom();
+            mapSeed = randomgen.Next(Int32.MaxValue - 1);
+            Console.WriteLine("Map Generated!");
+            var config = new NetPeerConfiguration("Multirogue Server") { Port = port };
+            server = new NetServer(config);
+            server.Start();
+            Console.WriteLine("Server Initialized!");
+            ClientHandler c = new ClientHandler(server, mapSeed);
+            clientHandler = new Thread(c.ClientMessageRecieverThread);
+            clientHandler.Start();
+            Console.WriteLine("Client Handler Initialized!");
         }
     }
 }
