a54c9090981815be1f11e1f38c15ef81396bd395
brandonbrown2 <brandonbrown@openmyr.com>
2017-01-17 01:51:19 -0500

    GameServer sanity-checks port argument

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TechDemo1;
-
+using TechDemo1.Map;
+using Lidgren;
 
 namespace GameServer
 {
@@ -18,11 +18,28 @@ namespace GameServer
                 Console.ReadKey();
                 return;
             }
-            //TODO: Create Map
-            //TODO: Init Server thread
-            //TODO: Init Incoming Data Handler thread
-            //TODO: Init Outgoing Data Handler thread
-            //TODO: Init Simulation(?)
+
+            int port;
+            if (!Int32.TryParse(args[0], out port))
+            {
+                Console.WriteLine("Bad port argument!");
+                Console.ReadKey();
+            }
+            else if (port > 0)
+            {
+                //TODO: Create Map
+                //TODO: Init Server thread
+                //TODO: Init Incoming Data Handler thread
+                //TODO: Init Outgoing Data Handler thread
+                //TODO: Init Simulation(?)
+                Console.WriteLine("Good port!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Port must be greater than zero!");
+                Console.ReadKey();
+            }
         }
     }
 }
